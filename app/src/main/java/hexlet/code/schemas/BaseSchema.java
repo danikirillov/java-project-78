9267ff9581eb048e887e36289f697e2c7ee68310
Daniil Kirillov <danikirillov@ya.ru>
2024-11-21 05:42:06 +0300

    fix checkstyle

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -8,6 +8,12 @@ public sealed class BaseSchema<T> permits MapSchema, NumberSchema, StringSchema 
     protected final Map<String, Predicate<T>> ruleIdToRule = new HashMap<>();
     protected Predicate<T> requiredRule = data -> ruleIdToRule.isEmpty() || data != null;
 
+    /**
+     * Validates an object using rules from the ruleIdToRule map.
+     *
+     * @param data object to validate
+     * @return true if all checks from the preconfigured schema passed
+     */
     public boolean isValid(T data) {
         var isValid = requiredRule.test(data);
 
